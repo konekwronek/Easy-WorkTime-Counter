f70e39a43f260f000ce0d094dbcc95af0e52a132
Konrad Wroński <konwron@gmail.com>
2021-06-01 14:15:25 +0200

    Gui and location changes.

--- Timer/FormMain.Designer.cs
+++ Timer/FormMain.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.buttonStart = new System.Windows.Forms.Button();
             this.numericUpDownRingTime = new System.Windows.Forms.NumericUpDown();
@@ -58,21 +59,14 @@
             // 
             // buttonStart
             // 
-            this.buttonStart.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.buttonStart.Location = new System.Drawing.Point(205, 8);
-            this.buttonStart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            resources.ApplyResources(this.buttonStart, "buttonStart");
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(71, 25);
-            this.buttonStart.TabIndex = 3;
-            this.buttonStart.Text = "Start";
             this.buttonStart.UseVisualStyleBackColor = true;
             this.buttonStart.Click += new System.EventHandler(this.button1_Click);
             // 
             // numericUpDownRingTime
             // 
-            this.numericUpDownRingTime.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.numericUpDownRingTime.Location = new System.Drawing.Point(116, 8);
-            this.numericUpDownRingTime.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            resources.ApplyResources(this.numericUpDownRingTime, "numericUpDownRingTime");
             this.numericUpDownRingTime.Maximum = new decimal(new int[] {
             60,
             0,
@@ -84,20 +78,13 @@
             0,
             -2147483648});
             this.numericUpDownRingTime.Name = "numericUpDownRingTime";
-            this.numericUpDownRingTime.Size = new System.Drawing.Size(40, 23);
-            this.numericUpDownRingTime.TabIndex = 1;
             this.numericUpDownRingTime.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             this.numericUpDownRingTime.Enter += new System.EventHandler(this.numericUpDown1_Enter);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.label1.Location = new System.Drawing.Point(6, 72);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 21);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Elapsed time:";
             // 
             // timer2
             // 
@@ -106,118 +93,70 @@
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.label2.Location = new System.Drawing.Point(8, 12);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 15);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Set Time:";
             // 
             // checkBoxRing
             // 
-            this.checkBoxRing.AutoSize = true;
-            this.checkBoxRing.Location = new System.Drawing.Point(70, 41);
-            this.checkBoxRing.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            resources.ApplyResources(this.checkBoxRing, "checkBoxRing");
             this.checkBoxRing.Name = "checkBoxRing";
-            this.checkBoxRing.Size = new System.Drawing.Size(50, 19);
-            this.checkBoxRing.TabIndex = 5;
-            this.checkBoxRing.Text = "Ring";
             this.checkBoxRing.UseVisualStyleBackColor = true;
             // 
             // checkBoxStop
             // 
-            this.checkBoxStop.AutoSize = true;
-            this.checkBoxStop.Location = new System.Drawing.Point(247, 41);
-            this.checkBoxStop.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            resources.ApplyResources(this.checkBoxStop, "checkBoxStop");
             this.checkBoxStop.Name = "checkBoxStop";
-            this.checkBoxStop.Size = new System.Drawing.Size(50, 19);
-            this.checkBoxStop.TabIndex = 7;
-            this.checkBoxStop.Text = "Stop";
             this.checkBoxStop.UseVisualStyleBackColor = true;
             // 
             // labelElapsedTime
             // 
-            this.labelElapsedTime.AutoSize = true;
-            this.labelElapsedTime.Font = new System.Drawing.Font("Segoe UI", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.labelElapsedTime.Location = new System.Drawing.Point(133, 65);
+            resources.ApplyResources(this.labelElapsedTime, "labelElapsedTime");
             this.labelElapsedTime.Name = "labelElapsedTime";
-            this.labelElapsedTime.Size = new System.Drawing.Size(119, 37);
-            this.labelElapsedTime.TabIndex = 6;
-            this.labelElapsedTime.Text = "00:00:00";
             // 
             // checkBoxReset
             // 
-            this.checkBoxReset.AutoSize = true;
-            this.checkBoxReset.Location = new System.Drawing.Point(308, 41);
-            this.checkBoxReset.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            resources.ApplyResources(this.checkBoxReset, "checkBoxReset");
             this.checkBoxReset.Name = "checkBoxReset";
-            this.checkBoxReset.Size = new System.Drawing.Size(54, 19);
-            this.checkBoxReset.TabIndex = 8;
-            this.checkBoxReset.Text = "Reset";
             this.checkBoxReset.UseVisualStyleBackColor = true;
             // 
             // buttonStopReset
             // 
-            this.buttonStopReset.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.buttonStopReset.Location = new System.Drawing.Point(281, 8);
-            this.buttonStopReset.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            resources.ApplyResources(this.buttonStopReset, "buttonStopReset");
             this.buttonStopReset.Name = "buttonStopReset";
-            this.buttonStopReset.Size = new System.Drawing.Size(110, 25);
-            this.buttonStopReset.TabIndex = 4;
-            this.buttonStopReset.Text = "Stop and Reset";
             this.buttonStopReset.UseVisualStyleBackColor = true;
             this.buttonStopReset.Click += new System.EventHandler(this.buttonStop_Click);
             // 
             // richTextBoxMessage
             // 
-            this.richTextBoxMessage.Location = new System.Drawing.Point(8, 109);
-            this.richTextBoxMessage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            resources.ApplyResources(this.richTextBoxMessage, "richTextBoxMessage");
             this.richTextBoxMessage.Name = "richTextBoxMessage";
-            this.richTextBoxMessage.Size = new System.Drawing.Size(383, 29);
-            this.richTextBoxMessage.TabIndex = 10;
-            this.richTextBoxMessage.Text = "Message";
             // 
             // checkTopMost
             // 
-            this.checkTopMost.AutoSize = true;
-            this.checkTopMost.Location = new System.Drawing.Point(269, 78);
-            this.checkTopMost.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            resources.ApplyResources(this.checkTopMost, "checkTopMost");
             this.checkTopMost.Name = "checkTopMost";
-            this.checkTopMost.Size = new System.Drawing.Size(92, 19);
-            this.checkTopMost.TabIndex = 9;
-            this.checkTopMost.Text = "Top Window";
             this.checkTopMost.UseVisualStyleBackColor = true;
             this.checkTopMost.CheckedChanged += new System.EventHandler(this.checkBoxMessage_CheckedChanged);
             // 
             // checkBoxMessage
             // 
-            this.checkBoxMessage.AutoSize = true;
-            this.checkBoxMessage.Location = new System.Drawing.Point(125, 41);
-            this.checkBoxMessage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            resources.ApplyResources(this.checkBoxMessage, "checkBoxMessage");
             this.checkBoxMessage.Name = "checkBoxMessage";
-            this.checkBoxMessage.Size = new System.Drawing.Size(104, 19);
-            this.checkBoxMessage.TabIndex = 6;
-            this.checkBoxMessage.Text = "Show Message";
             this.checkBoxMessage.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 42);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 15);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "On time:";
             // 
             // numericUpDownSeconds
             // 
+            resources.ApplyResources(this.numericUpDownSeconds, "numericUpDownSeconds");
             this.numericUpDownSeconds.Increment = new decimal(new int[] {
             5,
             0,
             0,
             0});
-            this.numericUpDownSeconds.Location = new System.Drawing.Point(159, 8);
             this.numericUpDownSeconds.Maximum = new decimal(new int[] {
             60,
             0,
@@ -229,24 +168,19 @@
             0,
             -2147483648});
             this.numericUpDownSeconds.Name = "numericUpDownSeconds";
-            this.numericUpDownSeconds.Size = new System.Drawing.Size(40, 23);
-            this.numericUpDownSeconds.TabIndex = 2;
             this.numericUpDownSeconds.ValueChanged += new System.EventHandler(this.numericUpDownSeconds_ValueChanged);
             // 
             // numericUpDownHours
             // 
-            this.numericUpDownHours.Location = new System.Drawing.Point(72, 8);
+            resources.ApplyResources(this.numericUpDownHours, "numericUpDownHours");
             this.numericUpDownHours.Name = "numericUpDownHours";
-            this.numericUpDownHours.Size = new System.Drawing.Size(40, 23);
-            this.numericUpDownHours.TabIndex = 0;
             this.numericUpDownHours.ValueChanged += new System.EventHandler(this.numericUpDownHours_ValueChanged);
             this.numericUpDownHours.Enter += new System.EventHandler(this.numericUpDownHours_Enter);
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(403, 146);
             this.Controls.Add(this.numericUpDownHours);
             this.Controls.Add(this.numericUpDownSeconds);
             this.Controls.Add(this.label3);
@@ -262,12 +196,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.numericUpDownRingTime);
             this.Controls.Add(this.buttonStart);
-            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "FormMain";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Easy WorkTime Counter - KonSoftware";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRingTime)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).EndInit();
